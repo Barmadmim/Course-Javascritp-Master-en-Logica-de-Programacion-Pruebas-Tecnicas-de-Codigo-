8fb04a3b12a74ffeb8079922cf0fca8e20b7f735
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 18:39:32 -0500

    Add New Data

--- Exercise-7-Number-Odd.js
+++ Exercise-7-Number-Odd.js
@@ -1,14 +1,23 @@
-/*Dados dos números, devolver cuantos números IMPARES hay entre ellos
+/*
+Enunciado Ejercisio 7: 
+Dados dos números, devolver cuantos números IMPARES hay entre ellos.
+
+Ejemplo:
+Impares(1, 100) Devolver 50
+
+Como hacerlo:
 -Función que reciba los dos numeros
 -Bucle desde el numero1 al numero2
 -Si el resto es distinto a 0, es impar
--Aumentar el contador*/
+-Aumentar el contador
+
+*/
 
 function impar(numero1, numero2){
-    let contador=0;
-    for(numero1; numero1<numero2; numero1++){
+    let contador = 0;
+    for( numero1; numero1 < numero2; numero1++ ){
 
-        if(numero1%2 !== 0){
+        if( numero1 % 2 !== 0){
             console.log(`El numero ${numero1} es impar`);
             contador++;
         }
@@ -17,4 +26,4 @@ function impar(numero1, numero2){
     return contador;
 }
 
-console.log(impar(1,100));
+console.log("Numeros Impares =", impar(1,100));
