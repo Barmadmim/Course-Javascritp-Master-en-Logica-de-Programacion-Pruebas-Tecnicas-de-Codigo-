950769887e0368662103e51dab0647dc6184af32
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 17:55:43 -0500

    Add Exercise-5-Porcent-Number.js

---
+++ Exercise-5-Porcent-Number.js
@@ -0,0 +1,13 @@
+/*¿Cuánto es el X por ciento de X numero?
+- Función con parametros porcentaje y numero
+- Crear una variable para la operación del porcentaje
+- Devolver string que responda a la cuestion del enunciado
+*/
+
+function tantoPorCiento(porcentaje, numero){
+    let operacion = (numero (porcentaje/100));
+
+    return `El ${porcentaje}% de ${numero} es ${operacion}`;
+}
+
+console.log(tantoPorCiento(50, 2));
