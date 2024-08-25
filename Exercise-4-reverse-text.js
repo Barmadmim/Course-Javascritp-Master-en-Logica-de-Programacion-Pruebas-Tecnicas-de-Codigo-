44d53ddbb01ea7c4fc16ab508160cc5d6bcddaa9
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 17:47:54 -0500

    Add Exercise-4-reserve-text.js

---
+++ Exercise-4-reverse-text.js
@@ -0,0 +1,25 @@
+/*Dada una cadena de texto, darle la vuelta e invertir el orden de 
+sus caracteres,
+sin usar métodos propios del lenguage, solo estructuras de control
+
+-Función con parámetro "texto"
+-Crear una variable para guardar la cadena invertida
+-Bucle que recorra el string y guarde los caracteres en la variable*/
+
+function invertir(texto){
+    let invertido = "";
+
+    //saca los valores. SI fuese los indices seria for in
+    for(letra of texto){
+        invertido = letra + invertido
+    }
+
+    return invertido;
+}
+
+console.log("texto invertido es igual a", invertir("BryanRamirez"));
+
+//Sin estructuras de control sería
+function invertida(texto){
+    return texto.split("").reserve().join();
+}
