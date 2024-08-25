6e74207e50fd08c4dade9797d256d75651655522
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 16:34:30 -0500

    Create Exercise-1-Multiplication-Table.js

---
+++ Exercise-1-Multiplication-Table.js
@@ -0,0 +1,23 @@
+
+*/
+Como hacerlo:
+-Funcion con parametro "numero"
+-Variable con texto encabezado
+-Bucle del 1 al 10
+-Concatenar a la variable string con multiplicacion y su resultado
+*/
+
+
+function tablaMultiplicar(numero){
+    let resultado = `#Tabla del ${numero} # \n`;
+
+    for(let i=1;i<=10;i++){
+        let multiplicacion = (i*numero);
+
+        resultado += `${i} x ${numero} = ${multiplicacion} \n`
+    }
+
+    return resultado;
+};
+
+console.log(tablaMultiplicar(5));
