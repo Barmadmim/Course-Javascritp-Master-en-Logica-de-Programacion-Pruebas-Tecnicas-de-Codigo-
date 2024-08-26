09eb94226352df58725cc37d9ddbb69a606db00b
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:46:54 -0500

    Complete Exercise-5

--- Exercise-5-Porcent-Number.js
+++ Exercise-5-Porcent-Number.js
@@ -1,4 +1,7 @@
-/*¿Cuánto es el X por ciento de X numero?
+/*
+Enunciado Ejercisio 5:
+¿Cuánto es el X por ciento de X numero?
+
 - Función con parametros porcentaje y numero
 - Crear una variable para la operación del porcentaje
 - Devolver string que responda a la cuestion del enunciado
