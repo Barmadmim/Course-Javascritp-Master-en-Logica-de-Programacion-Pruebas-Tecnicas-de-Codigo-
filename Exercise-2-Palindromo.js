51184660186fbe1197bec7066dca09d438fd7d8a
Bryan Andres Ramírez Márquez <barmdmin@gmail.com>
2024-08-25 20:43:48 -0500

    Complete Exercise-2

--- Exercise-2-Palindromo.js
+++ Exercise-2-Palindromo.js
@@ -1,13 +1,19 @@
-//Dada una cadena de texto, comprobar si es un palindromo o no. 
-//Los palíndromos son palabras que se leen igual aún estando invertidos. 
-/*Como hacerlo:
+/*
+Enunciado Ejercisio 2:
+Dada una cadena de texto, comprobar si es un palindromo o no. 
+
+Los palíndromos son palabras que se leen igual aún estando invertidos.
+
+Ejemplo:
+Palidromono(ana) devuelve true
+Como hacerlo:
 -Función con parámetro "texto"
 -Separar el texto en un array de letras
 -Darle la vuelta
 -Unir el array de letras en un string y comparar con el parámetro
 
 */
-function Palimdromo(text){
+function Palindromo(text){
     let invertido = text
     .split('') //pone la palabras separadas por ''
     .reverse() //Invierte la palabra
