f6a4c12e3c9d42db09307af9774773662f1461f4
Barmadmim <badadmim@gmail.com>
2024-11-21 17:35:55 -0500

    Commit

---
+++ Exercise-16.js
@@ -0,0 +1,12 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal (salvo excepciones).
+- Hay muchas soluciones validas para un mismo ejercicio.
+
+Enunciado
+
+*/
